c17537f2b61b174af427d8819e05e568475acd81
Michael Holmes <michaelholmes618@gmail.com>
2021-04-22 18:30:41 -0500

    use state Challenge Part 1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 
 function App() {
+  const [time, resetTime] = useState(new Date().toLocaleTimeString());
+
+  function getTime() {
+    resetTime(new Date().toLocaleTimeString());
+  }
+
   return (
     <div className="container">
-      <h1>TIME</h1>
-      <button>Get Time</button>
+      <h1>{time}</h1>
+      <button onClick={getTime}>Get Time</button>
     </div>
   );
 }
